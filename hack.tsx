f2491ea09f5790045681e911504de1f1b43f29ea
Alireza <103531312+alirezashon@users.noreply.github.com>
2023-07-09 18:25:18 +0330

    Update hack.tsx

--- hack.tsx
+++ hack.tsx
@@ -1,204 +1,261 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
+/** @format */
 
-// const URL = 'https://my.mobinnet.ir'; // Replace with your desired URL
-// const username = '09304721160'; // Replace with your username
-// const password = 'Alireza1380'; // Replace with your password
+// import { NextApiRequest, NextApiResponse } from 'next'
+// import puppeteer from 'puppeteer-extra'
+// import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
-// // Function to fetch the URL and get the HTML content
-// async function fetchURL(): Promise<string> {
+// puppeteer.use(StealthPlugin())
+
+// const loginURL = 'http://asset.mobinnet.net/Account/LDAPLogin'
+// const targetURL = 'http://asset.mobinnet.net/'
+
+// const main = async () => {
+//   const browser = await puppeteer.launch({ headless: true })
+//   const page = await browser.newPage()
+//   await page.goto(loginURL)
+
+//   await page.type('#UserName', 'al.akbari')
+//   await page.type('#Password', 'Argon&22')
+//   await page.click('[type="submit"]')
+
+//   await page.waitForNavigation({ waitUntil: 'networkidle0' })
+//   await page.goto(targetURL)
+
+//   await page.waitForSelector('.btn-asset')
+//   await page.click('.btn-asset')
+
+//   await page.click('#FormAssetFormViewbtnSave[type="submit"]')
+
+//   const spanTexts = await page.evaluate(() => {
+//     const spans = Array.from(document.querySelectorAll('label'))
+//     return spans.map(span => span.textContent)
+//   })
+
+//   await browser.close()
+
+//   return spanTexts
+// }
+
+// const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
 //   try {
-//     const response = await fetch(URL);
-//     const html = await response.text();
-//     return html;
+//     const spanTexts = await main()
+//     res.status(200).json({ message: 'Action performed successfully' , spanTexts})
 //   } catch (error) {
-//     console.error('Error fetching URL:', error);
-//     throw error;
+//     console.error('Error performing action:', error)
+//     res.status(500).json({ error: 'Error performing action' })
 //   }
 // }
 
-// // Function to perform the POST request with your data
-// async function performPostRequest(html: string): Promise<string> {
-//   // Use a library like Cheerio to parse the HTML and find the desired elements
-//   // For example, you can use Cheerio to find an input field's ID and make a POST request with your data
-
-//   // Replace the following code with your own logic using Cheerio to find the desired elements
-
-//   // Example: Finding an input field with ID 'username'
-//   const usernameElement = 'input#vs-input--username';
-
-//   // Example: Finding an input field with ID 'password'
-//   const passwordElement = 'input#vs-input--password';
-
-//   // Example: Finding the form and action URL
-//   const formAction = 'https://my.mobinnet.ir/Account/SignIn'; // Replace with your form action URL
-
-//   // Example: Creating the payload for the POST request
-//   const payload = {
-//     username,
-//     password
-//   };
-
-//   try {
-//     const response = await fetch(formAction, {
-//       method: 'POST',
-//       body: JSON.stringify(payload),
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     });
-
-//     const data = await response.text();
-//     console.log('POST request successful:', data);
-//     return data;
-//   } catch (error) {
-//     console.error('Error performing POST request:', error);
-//     throw error;
-//   }
-// }
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   try {
-//     const html = await fetchURL();
-//     const data = await performPostRequest(html);
-
-//     // Modify the code to format and send the data in the desired format (text, JSON, etc.)
-//     const responseData = { message: 'Success', data };
-
-//     res.status(200).json(responseData);
-//   } catch (error) {
-//     console.error('Error scraping website:', error);
-//     res.status(500).json({ message: 'Error scraping website' });
-//   }
-// }
+// export default handler
 
 
 
 
 
+import { NextApiRequest, NextApiResponse } from 'next'
+import { ElementHandle } from 'puppeteer'
+import path from 'path'
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import cheerio from 'cheerio';
+puppeteer.use(StealthPlugin())
 
-const URL = 'https://lms.iranian.ac.ir/login/index.php'; // Replace with your desired URL
-const TARGET_URL = 'https://lms.iranian.ac.ir/my/'; // Replace with the URL of the page you want to see after logging in
+const loginURL = 'http://asset.mobinnet.net/Account/LDAPLogin'
+const targetURL = 'http://asset.mobinnet.net/'
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const main = async () => {
+	const browser = await puppeteer.launch({ headless: false }) // Open a visible browser window
+	const page = await browser.newPage()
+	await page.goto(loginURL)
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    // Fetch the login page
-    const response = await fetch(URL);
-    const html = await response.text();
+	await page.type('#UserName', 'al.akbari')
+	await page.type('#Password', 'Argon&22')
+	await page.click('[type="submit"]')
 
-    // Load the HTML into Cheerio
-    const $ = cheerio.load(html);
+	await page.waitForNavigation({ waitUntil: 'networkidle0' })
+	await page.goto(targetURL)
 
-    // Get the logintoken value
-    const loginToken = $('input[name="logintoken"]').val("KeCxEWHOm6tAKvtQnR6qAxHlpQ9oupHS");
+	await page.waitForSelector('.btn-asset')
+	await page.click('.btn-asset')
 
-    // Find the input elements by ID and set their values
-    const usernameElement = $('#username');
-    const passwordElement = $('#password');
-    usernameElement.val('4011650025');
-    passwordElement.val('Alireza1380');
+	await page.waitForTimeout(3000) // Wait for the button action to complete, adjust the timeout value as needed
+	await page.click('a[onclick="FormAssetFormViewHeaderClick();"]')
+	await page.waitForTimeout(3000)
 
-    // Get the form action URL
-    const formElement = $('#login');
-    const formAction = formElement.attr('action');
+	const fileInput = (await page.$(
+		'#FormImportAssetFormView_UploadFile'
+	)) as ElementHandle<HTMLInputElement>
 
-    if (!formAction) {
-      throw new Error('Form action URL not found');
-    }
+	const filePath = path.resolve(__dirname, './alireza.csv')
+	if (fileInput) {
+		await fileInput.uploadFile(filePath) // Upload the file using the input element's uploadFile method
+	}
 
-    // Create the form data payload
-    const formData = new URLSearchParams();
-    formData.append('logintoken', String(loginToken));
-    formData.append('username', String(usernameElement.val() || ''));
-    formData.append('password', String(passwordElement.val() || ''));
+	await page.waitForTimeout(3000)
 
-    // Submit the form by sending a POST request
-    await fetch(formAction, {
-      method: 'POST',
-      body: formData,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    });
+	await page.waitForSelector('#FormImportAssetFormViewbtnSave')
+	await page.click('#FormImportAssetFormViewbtnSave')
 
-    // Wait for the action to complete and fetch the target page
-    await delay(2000); // Adjust the delay as per your needs or use more appropriate waiting methods
+	await page.waitForSelector('.analysis-icon.complete') // Wait for the analysis to be completed
+	await page.waitForTimeout(3000) // Wait for any additional tasks to complete
 
-    // Fetch the target page
-    const targetResponse = await fetch(TARGET_URL);
-    const targetHtml = await targetResponse.text();
+	const spanTexts = await page.evaluate(() => {
+		const spans = Array.from(document.querySelectorAll('label'))
+		return spans.map((span) => span.textContent)
+	})
 
-    // Load the target HTML into Cheerio
-    const $target = cheerio.load(targetHtml);
-
-    // Extract data or perform further operations based on the target page
-
-    // Example: Extracting data from a specific element
-    const resultElement = $target('#instance-20077-header'); // Adjust the selector based on the target page
-    const result = resultElement.text().trim(); // Trim any leading/trailing whitespace
-
-    // Return the result as the API response
-    res.status(200).json({ message: 'Action performed successfully', result, targetHtml });
-  } catch (error) {
-    console.error('Error performing action:', error);
-    res.status(500).json({ error: 'Error performing action' });
-  }
+	return spanTexts
 }
 
+const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+	try {
+		const spanTexts = await main()
+		res
+			.status(200)
+			.json({ message: 'Action performed successfully', spanTexts })
+	} catch (error) {
+		console.error('Error performing action:', error)
+		res.status(500).json({ error: 'Error performing action' })
+	}
+}
+
+export default handler
 
 
+// import { NextApiRequest, NextApiResponse } from 'next'
+// import { ElementHandle } from 'puppeteer'
+// import path from 'path'
+// import puppeteer from 'puppeteer-extra'
+// import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
+// puppeteer.use(StealthPlugin())
 
+// const loginURL = 'http://asset.mobinnet.net/Account/LDAPLogin'
+// const targetURL = 'http://asset.mobinnet.net/'
 
-// import { NextApiRequest, NextApiResponse } from "next"
+// const main = async () => {
+// 	const browser = await puppeteer.launch({ headless: false }) // Open a visible browser window
+// 	const page = await browser.newPage()
+// 	await page.goto(loginURL)
 
-// import cheerio from "cheerio"
+// 	await page.type('#UserName', 'al.akbari')
+// 	await page.type('#Password', 'Argon&22')
+// 	await page.click('[type="submit"]')
 
-// const URL = "https://lms.iranian.ac.ir/login/index.php" // Replace with your desired URL
+// 	await page.waitForNavigation({ waitUntil: 'networkidle0' })
+// 	await page.goto(targetURL)
 
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   try {
-//     // Fetch the page
-//     const response = await fetch(URL)
-//     const html = await response.text()
+// 	await page.waitForSelector('.btn-asset')
+// 	await page.click('.btn-asset')
 
-//     // Load the HTML into Cheerio
-//     const $ = cheerio.load(html)
+// 	await page.waitForTimeout(3000) // Wait for the button action to complete, adjust the timeout value as needed
+// 	await page.click('a[onclick="FormAssetFormViewHeaderClick();"]')
+// 	await page.waitForTimeout(3000)
 
-//     // Find the input element by ID and set its value
-//     const inputElement = $("#username")
-//     inputElement.val("4011650025")
-//     const pass = $("#password")
-//     pass.val("Alireza1380")
+// 	const fileInput = (await page.$(
+// 		'#FormImportAssetFormView_UploadFile'
+// 	)) as ElementHandle<HTMLInputElement>
 
-//     // Simulate a button click event
-//     const buttonElement = $("#loginbtn")
-//     const buttonClickScript = buttonElement.attr("onclick")
-//     if (buttonClickScript) {
-//       // Evaluate the button's onclick script to simulate the click event
-//       eval(buttonClickScript)
-//     }
+// 	const filePath = path.resolve(__dirname, './alireza.csv')
+// 	if (fileInput) {
+// 		await fileInput.uploadFile(filePath) // Upload the file using the input element's uploadFile method
+// 	}
 
-//     // Wait for the action to complete and fetch the updated page or handle the response
+// 	await page.waitForTimeout(3000)
 
-//     // Extract data or perform further operations based on the updated page or response
+// 	await page.waitForSelector('#FormImportAssetFormViewbtnSave')
+// 	await page.click('#FormImportAssetFormViewbtnSave')
 
-//     // Example: Extracting data from a specific element
-//     const resultElement = $("#instance-20077-header")
-//     const ali = $("a")
-//     const result = ali.text()
+// 	const spanTexts = await page.evaluate(() => {
+// 		const spans = Array.from(document.querySelectorAll('label'))
+// 		return spans.map((span) => span.textContent)
+// 	})
 
-//     // Return the result as the API response
-//     res.status(200).json({ message: "Action performed successfully", result })
-//   } catch (error) {
-//     console.error("Error performing action:", error)
-//     res.status(500).json({ error: "Error performing action" })
-//   }
+// 	return spanTexts
 // }
+
+// const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+// 	try {
+// 		const spanTexts = await main()
+// 		res
+// 			.status(200)
+// 			.json({ message: 'Action performed successfully', spanTexts })
+// 	} catch (error) {
+// 		console.error('Error performing action:', error)
+// 		res.status(500).json({ error: 'Error performing action' })
+// 	}
+// }
+
+// export default handler
+
+
+
+
+
+
+
+
+
+
+
+
+// import { NextApiRequest, NextApiResponse } from 'next'
+// import { ElementHandle } from 'puppeteer'
+// import puppeteer from 'puppeteer-extra'
+// import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+
+// puppeteer.use(StealthPlugin())
+
+// const loginURL = 'http://asset.mobinnet.net/Account/LDAPLogin'
+// const targetURL = 'http://asset.mobinnet.net/'
+
+// const main = async () => {
+// 	const browser = await puppeteer.launch({ headless: true })
+// 	const page = await browser.newPage()
+// 	await page.goto(loginURL)
+
+// 	await page.type('#UserName', 'al.akbari')
+// 	await page.type('#Password', 'Argon&22')
+// 	await page.click('[type="submit"]')
+
+// 	await page.waitForNavigation({ waitUntil: 'networkidle0' })
+// 	await page.goto(targetURL)
+
+// 	await page.waitForSelector('.btn-asset')
+// 	await page.click('.btn-asset')
+
+// 	await page.waitForTimeout(3000) // Wait for the button action to complete, adjust the timeout value as needed
+// 	await page.click('a[onclick="FormAssetFormViewHeaderClick();"]')
+// 	await page.waitForTimeout(3000)
+// 	const fileInput = (await page.$(
+// 		'#FormImportAssetFormView_UploadFile'
+// 	)) as ElementHandle<HTMLInputElement>
+
+// 		await fileInput.uploadFile('./alireza.csv') // Adjust the file path as needed
+	
+// 	await page.waitForSelector('#FormImportAssetFormViewbtnSave')
+// 	await page.click('#FormImportAssetFormViewbtnSave') // Wait for the form submission to complete, adjust the timeout value as needed
+
+// 	const spanTexts = await page.evaluate(() => {
+// 		const spans = Array.from(document.querySelectorAll('label'))
+// 		return spans.map((span) => span.textContent)
+// 	})
+
+// 	await browser.close()
+
+// 	return spanTexts
+// }
+
+// const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+// 	try {
+// 		const spanTexts = await main()
+// 		res
+// 			.status(200)
+// 			.json({ message: 'Action performed successfully', spanTexts })
+// 	} catch (error) {
+// 		console.error('Error performing action:', error)
+// 		res.status(500).json({ error: 'Error performing action' })
+// 	}
+// }
+
+// export default handler
